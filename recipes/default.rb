ed30aa1cffe601295206ac6ff70b557c0c165ea5
Bob Dean <radean@hotmail.com>
2017-11-03 10:06:37 -0400

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ec2-34-201-162-194.compute-1.amazonaws.com',
+    'ipaddress' => '34.201.162.194',
+    'port' => '80',
+    'ssl_port' => '80',
+}]
+
+include_recipe 'haproxy::manual'

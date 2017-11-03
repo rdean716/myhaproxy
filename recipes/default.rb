98590165082ff5b322610bc45709ac75c90a51dd
Bob Dean <radean@hotmail.com>
2017-11-03 10:51:27 -0400

    version 0.2.0, adds lb member

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,12 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-
 node.default['haproxy']['members'] = [{
     'hostname' => 'ec2-34-201-162-194.compute-1.amazonaws.com',
     'ipaddress' => '34.201.162.194',
-    'port' => '80',
-    'ssl_port' => '80',
+    'port' => 80,
+    'ssl_port' => 80,
+    },{
+    'hostname' => 'ec2-54-209-226-190.compute-1.amazonaws.com',
+    'ipaddress' => '54.209.226.190',
+    'port' => 80,
+    'ssl_port' => 80,
 }]
 
 include_recipe 'haproxy::manual'

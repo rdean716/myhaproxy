fdd1ffe9f2b6a38f538dbc481b6c5da14c671013
Bob Dean <radean@hotmail.com>
2017-11-03 14:42:10 -0400

    version 1.0, update the lb pool dynamically

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,16 +4,20 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-    'hostname' => 'ec2-34-201-162-194.compute-1.amazonaws.com',
-    'ipaddress' => '34.201.162.194',
-    'port' => 80,
-    'ssl_port' => 80,
-    },{
-    'hostname' => 'ec2-54-209-226-190.compute-1.amazonaws.com',
-    'ipaddress' => '54.209.226.190',
-    'port' => 80,
-    'ssl_port' => 80,
-}]
+all_web_nodes = search('node', 'role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+    member = {
+        'hostname' => web_node['cloud']['public_hostname'],
+        'ipaddress' => web_node['cloud']['public_ipv4'],
+        'port' => 80,
+        'ssl_port' => 80
+    }
+    members.push(member)
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
